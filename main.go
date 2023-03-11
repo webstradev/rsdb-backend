3a5dfada9aea886b31e1470d4686326e6071d30c
webstradev <e.s.westra.95@gmail.com>
2023-03-11 13:28:28 +0100

    Break out routes to seperate file closes #37

--- main.go
+++ main.go
@@ -10,16 +10,9 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/webstradev/rsdb-backend/auth"
-	"github.com/webstradev/rsdb-backend/controllers"
-	"github.com/webstradev/rsdb-backend/controllers/articles"
-	"github.com/webstradev/rsdb-backend/controllers/platforms"
-	"github.com/webstradev/rsdb-backend/controllers/projects"
-	"github.com/webstradev/rsdb-backend/controllers/users"
 	"github.com/webstradev/rsdb-backend/db"
-	"github.com/webstradev/rsdb-backend/middlewares"
 	"github.com/webstradev/rsdb-backend/migrations"
 	"github.com/webstradev/rsdb-backend/utils"
 )
@@ -56,73 +49,10 @@ func main() {
 		AuthService: auth.NewAuthService(),
 	}
 
-	// Initialise router
-	router := gin.New()
-
-	// Disable logging for health check endpoint
-	logger := gin.LoggerWithConfig(gin.LoggerConfig{SkipPaths: []string{"/api/health"}})
-
-	// Use the logger and recovery middleware
-	router.Use(logger, gin.Recovery())
-
-	// Health check for k8s
-	router.GET("/api/health", func(c *gin.Context) {
-		c.Status(http.StatusOK)
-	})
-
-	// Users (unauthenticated)
-	router.POST("/api/v1/login", users.Login(env))
-	router.POST("api/v1/users/register", users.Register(env))
-
-	// All the calls to the api group will require authentication
-	api := router.Group("/api/v1")
-	api.Use(middlewares.JWTAuthMiddleware(env))
-
-	// General
-	api.GET("/counts", controllers.GetCounts(env))
-
-	// Platforms
-	api.GET("/platforms", middlewares.PaginationMiddleware(), platforms.GetPlatforms(env))
-	api.POST("/platforms", platforms.CreatePlatform(env))
-	api.GET("/platforms/:platformId", platforms.GetPlatform(env))
-	api.PUT("/platforms/:platformId", platforms.EditPlatform(env))
-	api.DELETE("/platforms/:platformId", platforms.DeletePlatform(env))
-
-	// Contacts
-	api.GET("/platforms/:platformId/contacts", platforms.GetContacts(env))
-	api.POST("/platforms/:platformId/contacts", platforms.CreateContact(env))
-	api.PUT("/platforms/:platformId/contacts/:id", platforms.EditContact(env))
-	api.DELETE("/platforms/:platformId/contacts/:id", platforms.DeleteContact(env))
-
-	// Articles
-	api.GET("/articles", middlewares.PaginationMiddleware(), articles.GetArticles(env))
-	api.POST("/articles", articles.CreateArticle(env))
-	api.GET("/articles/:articleId", articles.GetArticle(env))
-	api.PUT("/articles/:articleId", articles.EditArticle(env))
-	api.DELETE("/articles/:articleId", articles.DeleteArticle(env))
-
-	// Projects
-	api.GET("/projects", middlewares.PaginationMiddleware(), projects.GetProjects(env))
-	api.POST("/projects", projects.CreateProject(env))
-	api.GET("/projects/:projectId", projects.GetProject(env))
-	api.PUT("/projects/:projectId", projects.EditProject(env))
-	api.DELETE("/projects/:projectId", projects.DeleteProject(env))
-
-	// Users (authenticated)
-	api.PUT("/users/password", users.EditPassword(env))
-
-	// Admin Routes
-	admin := api.Group("/admin")
-	admin.Use(middlewares.AdminAuthMiddleware())
-
-	// Users (admin)
-	admin.GET("/users/token", users.GetRegistrationToken(env))
-	admin.GET("/users/:userId/resettoken", users.GetPasswordResetToken(env))
-
 	// Server object
 	s := &http.Server{
 		Addr:         ":8080",
-		Handler:      router,
+		Handler:      registerRoutes(env),
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  2 * time.Second,
 		WriteTimeout: 2 * time.Second,
